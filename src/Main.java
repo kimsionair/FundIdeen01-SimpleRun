285e698b2591a473b62190014fe34fa37e1f5edf
Paul Libbrecht <paul@hoplahup.net>
2016-04-13 13:17:19 +0200

    a much simpler thing... to avoid sync problems for now. paul

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,21 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) throws Throwable{
 
-        Ellipse egg = new Ellipse(100, 100, 40, 60);
-        egg.setColor(Color.CYAN);
-        egg.fill();
+        System.out.println("Hier ist die Konsole. Willkommen!");
 
-        Thread.sleep(10);
-        egg.grow(10,10);
-        Thread.sleep(10);
-        egg.grow(10,10);
-        Thread.sleep(10);
-        egg.grow(10,10);
-        Thread.sleep(10);
-        egg.grow(10,10);
-        Thread.sleep(10);
-        egg.grow(10,10);
-        Thread.sleep(10);
-        egg.grow(10,10);
+        System.out.println("... hier wird alles ausgegeben, was mit System.out oder System.err gegeben wird.");
 
+        JOptionPane.showMessageDialog(new JFrame(), "Hi there, this is a dialog.");
+
+        System.out.println("Und wir machen weiter mit der Konsole.");
+
+        JOptionPane.showMessageDialog(new JFrame(), "Hi there, ich kann auch sprechen.");
+
+        System.out.println("Wie viele Dialogs waren das?");
+
+        System.out.println("Wie viele Konsole Zeilen?");
     }
 }
